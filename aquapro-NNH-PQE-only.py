634a9b63f5c50884f7c915e7c66b939e5a6d417f
Carrie Wang <61529499+Carrieww@users.noreply.github.com>
2024-10-06 16:35:12 +0800

    update NNH PQE naive

--- aquapro-NNH-PQE-only.py
+++ aquapro-NNH-PQE-only.py
@@ -1,29 +1,32 @@
-import time
-import numpy as np
-from numba import njit
-from pathlib import Path
-from scipy.stats import norm
 from math import floor, ceil
 from collections import defaultdict
-from hyper_parameter import norm_scale, std_offset
 
 from aquapro_util import (
     load_data,
     preprocess_dist,
     preprocess_topk_phi,
-    preprocess_sync,
+    preprocess_ranks,
 )
 from aquapro_util import (
     array_union,
     set_diff,
 )
 
+from numba import njit
+from hyper_parameter import std_offset
+import numpy as np
+
+# import pickle
+from pathlib import Path
+
+import time
+
 
 @njit
 def test_PQA_PT(oracle_dist, phi, topk, t=0.9, prob=0.9, pt=0.9, pilots=None):
     true_ans = np.where(oracle_dist <= t)[0]
     if len(true_ans) == 0:
-        return 0, 0, 0
+        return 0, 0, 0, np.empty(0, dtype=np.int64)
 
     pbs = np.zeros(len(phi) + 1)
     k_star = 0
@@ -43,7 +46,7 @@ def test_PQA_PT(oracle_dist, phi, topk, t=0.9, prob=0.9, pt=0.9, pilots=None):
             k_star = i
 
     if k_star == 0:
-        return 1, 0, 0
+        return 0, 0, 0, np.empty(0, dtype=np.int64)
 
     if pilots is None:
         ans = topk[:k_star]
@@ -55,14 +58,14 @@ def test_PQA_PT(oracle_dist, phi, topk, t=0.9, prob=0.9, pt=0.9, pilots=None):
     precision = true_pos / len(ans)
     recall = true_pos / len(true_ans)
 
-    return precision, recall, k_star
+    return precision, recall, k_star, ans
 
 
 @njit
 def test_PQA_RT(oracle_dist, phi, topk, t=0.9, prob=0.9, rt=0.9, pilots=None):
     true_ans = np.where(oracle_dist <= t)[0]
     if len(true_ans) == 0:
-        return 0, 0, len(oracle_dist)
+        return 0, 0, len(oracle_dist), np.empty(0, dtype=np.int64)
 
     L = 1
     R = len(phi)
@@ -120,7 +123,7 @@ def test_PQA_RT(oracle_dist, phi, topk, t=0.9, prob=0.9, rt=0.9, pilots=None):
     precision = true_pos / len(ans)
     recall = true_pos / len(true_ans)
 
-    return precision, recall, k_star
+    return precision, recall, k_star, ans
 
 
 def test_PQE_PT(oracle_dist, proxy_dist, bd, t=0.9, prob=0.9, pt=0.9):
@@ -128,14 +131,30 @@ def test_PQE_PT(oracle_dist, proxy_dist, bd, t=0.9, prob=0.9, pt=0.9):
     samples = np.random.choice(len(oracle_dist), size=bd, replace=False, p=imp_p)
 
     est_scale = np.std(oracle_dist[samples] - proxy_dist[samples]) + std_offset
+    print(f"norm scale is {est_scale}")
 
     topk, phi = preprocess_topk_phi(proxy_dist, norm_scale=est_scale, t=t)
 
-    precision, recall, _ = test_PQA_PT(
+    # # Step 3: Identify low-confidence points
+    # low_confidence_points = np.where(phi < 0.8)[0]
+
+    # # # Step 4: Sample from low-confidence points if enough points exist
+    # if len(low_confidence_points) >= bd:
+    #     samples = np.random.choice(low_confidence_points, size=bd, replace=False)
+    # else:
+    #     # If not enough low-confidence points, add some high-confidence points
+    #     high_confidence_points = np.where(phi >= 0.8)[0]
+    #     additional_samples = np.random.choice(
+    #         high_confidence_points, size=bd - len(low_confidence_points), replace=False
+    #     )
+    #     samples = np.concatenate([low_confidence_points, additional_samples])
+
+    # Step 5: Continue as usual with the precision and recall testing
+    precision, recall, _, ans = test_PQA_PT(
         oracle_dist, phi, topk, t=t, prob=prob, pt=pt, pilots=samples
     )
 
-    return precision, recall, _
+    return precision, recall, _, ans
 
 
 def test_PQE_RT(oracle_dist, proxy_dist, bd, t=0.9, prob=0.9, rt=0.9):
@@ -146,24 +165,36 @@ def test_PQE_RT(oracle_dist, proxy_dist, bd, t=0.9, prob=0.9, rt=0.9):
 
     topk, phi = preprocess_topk_phi(proxy_dist, norm_scale=est_scale, t=t)
 
-    precision, recall, _ = test_PQA_RT(
+    # # Step 3: Identify low-confidence points
+    # low_confidence_points = np.where(phi < 0.8)[0]
+
+    # # # Step 4: Sample from low-confidence points if enough points exist
+    # if len(low_confidence_points) >= bd:
+    #     samples = np.random.choice(low_confidence_points, size=bd, replace=False)
+    # else:
+    #     # If not enough low-confidence points, add some high-confidence points
+    #     high_confidence_points = np.where(phi >= 0.8)[0]
+    #     additional_samples = np.random.choice(
+    #         high_confidence_points, size=bd - len(low_confidence_points), replace=False
+    #     )
+    #     samples = np.concatenate([low_confidence_points, additional_samples])
+
+    # Step 5: Continue as usual with the precision and recall testing
+
+    precision, recall, _, ans = test_PQA_RT(
         oracle_dist, phi, topk, t=t, prob=prob, rt=rt, pilots=samples
     )
 
-    return precision, recall, _
+    return precision, recall, _, ans
 
 
-def HT_acc(name, ans, total, op, GT, prop_default=None):
+def HT_acc(name, ans, total, op, GT, prop_c):
     print(f"finished {name} algorithm for q")
     print(f"FRNN result: {len(ans)}")
     print(f"total patients: {total}")
 
-    if prop_default:
-        prop = prop_default
-    else:
-        prop = len(ans) / total
-    print(f"D proportion or c: {prop}; approx: {len(ans) / total}")
-    z_stat, p_value, reject = one_proportion_z_test(len(ans), total, prop, 0.05, op)
+    print(f"c proportion: {prop_c}; approx: {len(ans) / total}")
+    z_stat, p_value, reject = one_proportion_z_test(len(ans), total, prop_c, 0.05, op)
 
     print("Z-Statistic:", z_stat)
     print("P-Value:", p_value)
@@ -173,6 +204,9 @@ def HT_acc(name, ans, total, op, GT, prop_default=None):
     print("align:", align)
 
     return align, reject
+
+
+from scipy.stats import norm
 
 
 def one_proportion_z_test(
@@ -221,124 +255,155 @@ def one_proportion_z_test(
 if __name__ == "__main__":
     start_time = time.time()
     Fname = "icd9_eICU"
-    Proxy, Oracle = load_data(name=Fname)
+    Proxy_emb, Oracle_emb = load_data(name=Fname)
 
     # NN algo parameters
+    # Pt = Rt = 0.72
     Prob = 0.95
     Dist_t = 0.85
-    H1_op = "greater"
-    for seed in [1, 2, 3, 4]:
-        print(f"Prob: {Prob}; r: {Dist_t}; seed: {seed}")
+    H1_op = "less"
+    seed = 1
+    print(f"Prob: {Prob}; r: {Dist_t}; seed: {seed}")
 
-        num_query = 1
-        np.random.seed(seed)
-        Index = np.random.choice(range(len(Oracle)), size=num_query, replace=False)
+    num_query = 1
+    num_sample = 30
 
-        fac_list = np.arange(0.8, 1.25, 0.05)
-        fac_list = [round(num, 4) for num in fac_list]
+    np.random.seed(seed)
+    Index = np.random.choice(range(len(Oracle_emb)), size=num_query, replace=False)
 
-        # res = defaultdict(list)
+    fac_list = np.arange(0.8, 1.25, 5)
+    fac_list = [round(num, 4) for num in fac_list]
 
-        Proxy_dist, _ = preprocess_dist(Oracle, Proxy, Oracle[[Index[0]]])
-        sync_Oracle = preprocess_sync(Proxy_dist, norm_scale)
-        true_ans_D = np.where(sync_Oracle <= Dist_t)[0]
-        print(f"the GT proportion is {(len(true_ans_D) / Oracle.shape[0])}")
+    Proxy_dist, Oracle_dist = preprocess_dist(
+        Oracle_emb, Proxy_emb, Oracle_emb[[Index[0]]]
+    )
+    # Ranks = preprocess_ranks(Proxy_dist)
+    true_ans_D = np.where(Oracle_dist <= Dist_t)[0]
 
-        rt_k_precision = defaultdict(list)
-        rt_k_recall = defaultdict(list)
-        rt_k_acc = defaultdict(list)
-        rt_k_rejH0 = defaultdict(list)
-        rt_response_time = defaultdict(list)
-        rt_prop = defaultdict(list)
-        rt_cost = defaultdict(list)
+    prop_D = len(true_ans_D) / Oracle_dist.shape[0]
+    print(f"the GT proportion is {(prop_D)}")
 
-        # PQA-RT-sync
-        RT_start = time.time()
-        topk, phi = preprocess_topk_phi(Proxy_dist, norm_scale=norm_scale, t=Dist_t)
-        oracle_call_set = set()
-        epsilon = 0.01
-        itr = 0
-        rt_max = 0.8
-        rt_min = 0
-        while itr < 20:
-            print(f">>> iteration for finding t*: {itr}")
-            rt = (rt_max + rt_min) / 2
-            print(f"rt is {rt}")
+    for cost in [200, 400, 600, 800]:
+        for Pt in [0.95]:
+            rt_k_success = defaultdict(list)
+            rt_k_precision = defaultdict(list)
+            rt_k_recall = defaultdict(list)
+            rt_k_acc = defaultdict(list)
+            rt_k_rejH0 = defaultdict(list)
+            rt_response_time = defaultdict(list)
+            rt_prop = defaultdict(list)
+            rt_cost = defaultdict(list)
 
-            _, _, k_star = test_PQA_RT(
-                sync_Oracle, phi, topk, t=Dist_t, prob=Prob, rt=rt
+            pt_k_success = defaultdict(list)
+            pt_k_recall = defaultdict(list)
+            pt_k_precision = defaultdict(list)
+            pt_k_acc = defaultdict(list)
+            pt_k_rejH0 = defaultdict(list)
+            pt_response_time = defaultdict(list)
+            pt_prop = defaultdict(list)
+            pt_cost = defaultdict(list)
+            print(f"Pt=Rt={Pt}")
+            Rt = Pt
+
+            # PQE-PT
+            PT_start = time.time()
+            PT_precision, PT_recall, PT_k_star, PT_ans = test_PQE_PT(
+                Oracle_dist, Proxy_dist, bd=cost, t=Dist_t, prob=Prob, pt=Pt
             )
-            k_hat = int(k_star * (1 + 0 / 100))
-            RT_ans = topk[:k_hat]
-            oracle_call_set.update(RT_ans)
-            true_pos = len(np.intersect1d(RT_ans, true_ans_D))
+            PT_end = time.time()
 
-            precision = true_pos / len(RT_ans)
-            print(f"precision is {precision}")
-            if precision > rt:
-                rt_min = rt
-            else:
-                rt_max = rt
-
-            if abs(precision - rt) < epsilon:
-                break
-            else:
-                itr += 1
-        recall = true_pos / len(true_ans_D)
-        response_time = round(time.time() - RT_start, 2)
-
-        for fac in fac_list:
-            c_time_GT = (len(true_ans_D) / sync_Oracle.shape[0]) * fac
-            print(f"H1: % NN w.r.t {Index} is {H1_op} {c_time_GT}")
-            _, _, GT = one_proportion_z_test(
-                len(true_ans_D), sync_Oracle.shape[0], c_time_GT, 0.05, H1_op
+            # PQE-RT
+            RT_start = time.time()
+            RT_precision, RT_recall, RT_k_star, RT_ans = test_PQE_RT(
+                Oracle_dist,
+                Proxy_dist,
+                bd=cost,
+                t=Dist_t,
+                prob=Prob,
+                rt=Rt,
             )
-            print(f"the ground truth to reject H0 result is : {GT}")
-            align, reject = HT_acc(
-                "PQA-RT",
-                RT_ans,
-                sync_Oracle.shape[0],
-                H1_op,
-                GT,
-                prop_default=c_time_GT,
-            )
-            rt_k_acc[fac].append(align)
-            rt_k_rejH0[fac].append(reject)
-            rt_k_precision[fac].append(precision)
-            rt_k_recall[fac].append(recall)
-            rt_cost[fac].append(len(oracle_call_set))
-            rt_response_time[fac].append(response_time)
-            rt_prop[fac].append(len(RT_ans) / sync_Oracle.shape[0])
+            RT_end = time.time()
 
-        print("================================")
+            for fac in fac_list:
+                c_time_GT = (len(true_ans_D) / Oracle_dist.shape[0]) * fac
+                print(f">>> c is {c_time_GT}")
+                _, _, GT = one_proportion_z_test(
+                    len(true_ans_D),
+                    Oracle_dist.shape[0],
+                    c_time_GT,
+                    0.05,
+                    H1_op,
+                )
+                print(f"the ground truth to reject H0 result is : {GT}")
+                rt_align, rt_reject = HT_acc(
+                    "PQE-RT",
+                    RT_ans,
+                    Oracle_dist.shape[0],
+                    H1_op,
+                    GT,
+                    c_time_GT,
+                )
+                rt_k_acc[fac].append(rt_align)
+                rt_k_rejH0[fac].append(rt_reject)
+                rt_k_success[fac].append(int(RT_recall >= Rt))
+                rt_k_recall[fac].append(RT_recall)
+                rt_k_precision[fac].append(RT_precision)
+                rt_response_time[fac].append(round(RT_end - RT_start, 4))
+                rt_cost[fac].append(cost)
+                rt_prop[fac].append(len(RT_ans) / Oracle_dist.shape[0])
 
-        Path(f"./results_NNH/PQA-better1/").mkdir(parents=True, exist_ok=True)
-        for fac in fac_list:
-            backup_res = [
-                rt,
-                seed,
-                fac,
-                round(np.mean(rt_cost[fac]), 4),
-                round(np.mean(rt_k_recall[fac]), 4),
-                round(np.mean(rt_k_precision[fac]), 4),
-                round(np.mean(rt_k_acc[fac]), 4),
-                round(np.mean(rt_k_rejH0[fac]), 4),
-                round(np.mean(rt_response_time[fac]), 4),
-                round(np.mean(rt_prop[fac]), 4),
-            ]
-            with open(
-                f"results_NNH/PQA-better1/"
-                + Fname
-                + "_"
-                + H1_op
-                + f"_0927_version2.txt",
-                "a",
-            ) as file:
-                results_str = "\t".join(map(str, backup_res)) + "\n"
-                file.write(results_str)
+                pt_align, pt_reject = HT_acc(
+                    "PQE-PT",
+                    PT_ans,
+                    Oracle_dist.shape[0],
+                    H1_op,
+                    GT,
+                    c_time_GT,
+                )
+                pt_k_acc[fac].append(pt_align)
+                pt_k_rejH0[fac].append(pt_reject)
+                pt_k_success[fac].append(int(PT_recall >= Pt))
+                pt_k_recall[fac].append(PT_recall)
+                pt_k_precision[fac].append(PT_precision)
+                pt_response_time[fac].append(round(PT_end - PT_start, 4))
+                pt_cost[fac].append(cost)
+                pt_prop[fac].append(len(PT_ans) / Oracle_dist.shape[0])
 
-        # results_str = "\t".join(map(str, backup_res)) + "\n"
-        # print(results_str)
+            Path(f"./results_NNH/PQE/").mkdir(parents=True, exist_ok=True)
+            # print("final result is:")
+            for fac in fac_list:
+                backup_res = [
+                    Pt,
+                    fac,
+                    round(sum(pt_k_recall[fac]) / len(pt_k_recall[fac]), 4),
+                    round(sum(pt_k_precision[fac]) / len(pt_k_precision[fac]), 4),
+                    round(sum(pt_k_success[fac]) / len(pt_k_success[fac]), 4),
+                    round(sum(pt_k_acc[fac]) / len(pt_k_acc[fac]), 4),
+                    round(sum(pt_k_rejH0[fac]) / len(pt_k_rejH0[fac]), 4),
+                    round(sum(pt_response_time[fac]) / len(pt_response_time[fac]), 4),
+                    round(sum(pt_prop[fac]) / len(pt_prop[fac]), 4),
+                    round(sum(pt_cost[fac]) / len(pt_cost[fac]), 4),
+                    round(sum(rt_k_recall[fac]) / len(rt_k_recall[fac]), 4),
+                    round(sum(rt_k_precision[fac]) / len(rt_k_precision[fac]), 4),
+                    round(sum(rt_k_success[fac]) / len(rt_k_success[fac]), 4),
+                    round(sum(rt_k_acc[fac]) / len(rt_k_acc[fac]), 4),
+                    round(sum(rt_k_rejH0[fac]) / len(rt_k_rejH0[fac]), 4),
+                    round(sum(rt_response_time[fac]) / len(rt_response_time[fac]), 4),
+                    round(sum(rt_prop[fac]) / len(rt_prop[fac]), 4),
+                    round(sum(rt_cost[fac]) / len(rt_cost[fac]), 4),
+                ]
+                with open(
+                    f"results_NNH/PQE/"
+                    + Fname
+                    + "_"
+                    + H1_op
+                    + f"_query{str(seed)}_0921_30.txt",
+                    "a",
+                ) as file:
+                    results_str = "\t".join(map(str, backup_res)) + "\n"
+                    file.write(results_str)
+                # results_str = "\t".join(map(str, backup_res)) + "\n"
+                # print(results_str)
 
     end_time = time.time()
     print("execution time is %.2fs" % (end_time - start_time))
