35d5c79786492541132fa5d95a87951bac868a32
Carrie Wang <61529499+Carrieww@users.noreply.github.com>
2025-02-26 13:48:53 +0800

    update myscript

--- myscript.sh
+++ myscript.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 
-python_script="/home/ywang/predict_ht/SPRinT.py"
+python_script="./SPRinT.py"
 
 # Define parameter values
-agg_list=("avg" "pct" "sum" "var")
+agg_list=("pct" "avg" "sum" "var")
 
-fname_list=("icd9_eICU" "icd9_mimic" "Health_and_Household" "Electronics" "night-street")
-algo_list=("SPRinT" "PQA-PT" "PQA-RT" "SUPG-PT" "SUPG-RT" "TopK")
-version_name="test0217"
+fname_list=("Jigsaw") # "eICU" "MIMIC-III" "Jackson" "Jigsaw"
+algo_list=("SPRinT")  # "SUPG-PT" "SUPG-RT" "PQA-PT" "PQA-RT" "TopK"
+file_suffix="test"
+scalability_factor=0
 
 # Define attributes, IDs, beta values, and cost settings for each dataset
 declare -A attr_map
 declare -A attr_id_map
 declare -A beta_map
-declare -A initial_cost_map
-declare -A total_cost_map
+declare -A s_p_map
+declare -A s_map
 
 # Attribute mappings
-attr_map["icd9_eICU"]="age"
-attr_map["icd9_mimic"]="HeartRate"
-attr_map["Health_and_Household"]="ratings"
-attr_map["Electronics"]="ratings"
-attr_map["night-street"]="carSpeed"
+attr_map["eICU"]="age"
+attr_map["MIMIC-III"]="HeartRate"
+attr_map["Amazon-HH"]="ratings"
+attr_map["Amazon-E"]="ratings"
+attr_map["Jackson"]="carSpeed"
+attr_map["Jigsaw"]="downvote"
 
 # Attribute ID mappings
-attr_id_map["icd9_eICU"]=1
-attr_id_map["icd9_mimic"]=0
-attr_id_map["Health_and_Household"]=0
-attr_id_map["Electronics"]=0
-attr_id_map["night-street"]=0
+attr_id_map["eICU"]=1
+attr_id_map["MIMIC-III"]=0
+attr_id_map["Amazon-HH"]=0
+attr_id_map["Amazon-E"]=0
+attr_id_map["Jackson"]=0
+attr_id_map["Jigsaw"]=0
 
 # Beta mappings based on agg function
 beta_map["avg"]=0.5
@@ -37,20 +40,21 @@ beta_map["pct"]=1
 beta_map["sum"]=1
 
 # Cost mappings based on dataset
-initial_cost_map["icd9_mimic"]=150
-total_cost_map["icd9_mimic"]=500
+s_p_map["MIMIC-III"]=150
+s_map["MIMIC-III"]=500
 
-initial_cost_map["Electronics"]=1200
-total_cost_map["Electronics"]=2000
+s_p_map["Amazon-E"]=1200
+s_map["Amazon-E"]=2000
 
-initial_cost_map["Health_and_Household"]=2000
-total_cost_map["Health_and_Household"]=3000
+s_p_map["Amazon-HH"]=2000
+s_map["Amazon-HH"]=3000
+
 
 # Default cost values for other datasets
 for fname in "${fname_list[@]}"; do
-    if [[ -z "${initial_cost_map[$fname]}" ]]; then
-        initial_cost_map[$fname]=600
-        total_cost_map[$fname]=1000
+    if [[ -z "${s_p_map[$fname]}" ]]; then
+        s_p_map[$fname]=600
+        s_map[$fname]=1000
     fi
 done
 
@@ -68,20 +72,29 @@ for algo in "${algo_list[@]}"; do
         beta=${beta_map[$agg]}
 
         for fname in "${fname_list[@]}"; do
-            # Get the correct attr, attr_id, initial_cost, and total_cost
+            # Set Dist_t based on the dataset name
+            if [[ "$fname" == "Jigsaw" ]]; then
+                Dist_t=0.3
+            elif [[ "$fname" == "MIMIC-III" ]]; then
+                Dist_t=0.9
+            else
+                Dist_t=0.8
+            fi
+
+            # Get the correct attr, attr_id, s_p, and s
             attr=${attr_map[$fname]}
             attr_id=${attr_id_map[$fname]}
-            initial_cost=${initial_cost_map[$fname]}
-            total_cost=${total_cost_map[$fname]}
+            s_p=${s_p_map[$fname]}
+            s=${s_map[$fname]}
 
             # Define log file name
-            log_file="./results/${algo}/${agg}_${fname}_${version_name}.log"
+            log_file="./results/${algo}/${agg}_${fname}_${file_suffix}.log"
 
             # Create results directory if not exists
             mkdir -p "./results/${algo}"
 
             # Run the Python script with nohup
-            echo "Running: algo=$algo, agg=$agg, hypothesis_type=$hypothesis_type, Fname=$fname, attr=$attr, attr_id=$attr_id, beta=$beta, initial_cost=$initial_cost, total_cost=$total_cost"
+            echo "Running: algo=$algo, agg=$agg, hypothesis_type=$hypothesis_type, Fname=$fname, attr=$attr, attr_id=$attr_id, beta=$beta, scalability_factor=$scalability_factor, s_p=$s_p, s=$s"
             nohup ./venv/bin/python3 "$python_script" \
                 --algo "$algo" \
                 --agg "$agg" \
@@ -90,9 +103,11 @@ for algo in "${algo_list[@]}"; do
                 --attr "$attr" \
                 --attr_id "$attr_id" \
                 --beta "$beta" \
-                --version "$version" \
-                --initial_cost "$initial_cost" \
-                --total_cost "$total_cost" > "$log_file" 2>&1 &
+                --Dist_t "$Dist_t" \
+                --scalability_factor "$scalability_factor" \
+                --file_suffix "$file_suffix" \
+                --s_p "$s_p" \
+                --s "$s" > "$log_file" 2>&1 &
         done
     done
 done
